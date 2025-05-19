cda7e8f8ab235e735f9f67d2831d51446f81b6d7
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-19 17:40:39 -0300

    ✨ feat: implementa as rotas do projetos

---
+++ src/routers/projectRouters.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+
+router.get("/projetos", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views/projects.html"));
+});
+
+module.exports = router;
