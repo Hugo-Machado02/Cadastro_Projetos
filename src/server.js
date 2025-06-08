dff8bfa6e4a5354247d6f3ec836556115588e97b
Hugo Machado <hugo_machado02@hotmail.com>
2025-06-08 20:38:45 -0300

    ✨ feat: Atualiza configurações do server JS

--- src/server.js
+++ src/server.js
@@ -1,19 +1,48 @@
 const express = require("express");
 const cors = require("cors");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 const apiRoutes = require("./routers/configRouters");
 const path = require("path");
 require("dotenv").config({ path: ".env" });
-const create = require("./db/createTables");
+const sequelize = require("./db/connection");
 
-create();
+const { startDatabase } = require("./db/startDb");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: process.env.URL_CLIENT,
+    credentials: true, // Essencial para permitir o envio de cookies de sessão
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: process.env.NODE_ENV === 'production',
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24
+    },
+}));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'public', 'views')); 
+
+app.use('/static', express.static(path.join(__dirname, 'public', 'static')));
 app.use("/", apiRoutes);
 
-const service = app.listen(process.env.PORT, () => {
-  console.log(`Servidor rodando na porta ${service.address().port}`);
-});
+startDatabase()
+    .then(() => {
+        const service = app.listen(process.env.PORT, () => {
+            console.log(`Servidor rodando na porta ${service.address().port}`);
+            console.log('Modelos User e Project sincronizados com o banco de dados.');
+        });
+    })
+    .catch(error => {
+        console.error('Erro ao sincronizar o banco de dados:', error);
+    });
