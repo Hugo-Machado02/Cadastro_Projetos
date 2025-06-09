0cec0ad590cb50acfa2b7f35b846e56a1d764fc0
Hugo Machado <hugo_machado02@hotmail.com>
2025-06-09 17:29:52 -0300

    ✨ feat: Implementa Cookies no sistema

--- src/public/static/js/login.js
+++ src/public/static/js/login.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const formLogin = document.getElementById('formLogin');
+    const emailInput = document.getElementById('email');
+    const rememberMeCheckbox = document.getElementById('rememberMe');
+    const btnPassword = document.getElementById('visualizacaoLogin');
+    const savedEmail = getCookie('rememberedEmail');
+
+    verificaCheck()
 
     formLogin.addEventListener('submit', function(event) {
         event.preventDefault();
@@ -7,6 +13,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
         loginAuth(data);
     });
+
+    btnPassword.addEventListener('click', function() {
+        const passwordInput = document.getElementById('password');
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            btnPassword.textContent = 'visibility_off';
+        } else {
+            passwordInput.type = 'password';
+            btnPassword.textContent = 'visibility';
+        }
+    });
+
+    function verificaCheck(){
+        if (savedEmail) {
+            emailInput.value = savedEmail;
+            rememberMeCheckbox.checked = true;
+        }
+    }
 
     function loginAuth(dados){
         limpaFeedbacks();
@@ -27,10 +51,48 @@ document.addEventListener('DOMContentLoaded', function() {
                 });
                 return;
             }
+            if (dados.rememberMe) {
+                setCookie('rememberedEmail', dados.email, 30);
+            } else {
+                eraseCookie('rememberedEmail');
+            }
             return window.location.href = "/"
         })
         .catch(error => {
             console.error('Erro ao enviar os dados:', error);
         });
     }
+
+    function setCookie(name, value, days) {
+        let expiration = "";
+        if (days) {
+            const date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expiration = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expiration + "; path=/; SameSite=Lax";
+    }
+
+    function getCookie(email) {
+        const getEmail = email + "=";
+        const cookie = document.cookie.split(';');
+        for(let i=0; i < cookie.length; i++) {
+            let emailCookie = cookie[i];
+            while (emailCookie.charAt(0) === ' ') emailCookie = emailCookie.substring(1, emailCookie.length);
+            if (emailCookie.indexOf(getEmail) === 0) return emailCookie.substring(getEmail.length, emailCookie.length);
+        }
+        return null;
+    }
+
+    function eraseCookie(name) {
+        document.cookie = name + '=; Max-Age=-99999999; path=/;';
+    }
+
+    formLogin.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const data = getForm(formLogin);
+
+        loginAuth(data);
+    });
+
 });
