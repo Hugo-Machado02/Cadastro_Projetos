3cf561f1c8ecb762692d0c21648f4b55fff43e9d
Hugo Machado <hugo_machado02@hotmail.com>
2025-06-09 17:26:09 -0300

    ✨ feat: Implementa novas rotas de acesso a API

--- src/routers/projectRouters.js
+++ src/routers/projectRouters.js
@@ -7,8 +7,15 @@ router.get("/projetos", (req, res) => {
   const userName = req.session.user.name;
   res.render("projetos", { userName: userName });
 });
+router.get("/projetos/:id", (req, res) => {
+  const userName = req.session.user.name;
+  res.render("editaProjeto", { userName: userName });
+});
 
 router.get("/api/projetos/getProjetos", projectController.getAllProject);
+router.get("/api/projects/:id", projectController.getProjectId);
 router.post("/api/projetos/new", projectController.addProject);
+router.put("/api/projects/update/:id", projectController.updateProject);
+router.delete("/api/projetos/delete/:id", projectController.deleteProject);
 
 module.exports = router;
