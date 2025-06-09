2b9897972a0be3f1501f3d8a397f36e21de3648f
Hugo Machado <hugo_machado02@hotmail.com>
2025-06-09 17:25:30 -0300

    ✨ feat: Implementa Funcionalidade de Edição

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -6,6 +6,14 @@ class UserService{
         return await UserRepository.getAllUser();
     }
 
+    async getUsersActive(){
+        return await UserRepository.getUserActive()
+    }
+
+    async getUserId(id){
+        return await UserRepository.getUserId(id);
+    }
+
     async addUser(data){
         if(!this.#emailValidate(data.email)){
             return {status: 422, error: "Email inválido"}
@@ -22,11 +30,81 @@ class UserService{
         
         const newUser = await UserRepository.createUser(data);
 
-        if(!newUser.sucess){
+        if(!newUser.success){
             return {status: 500, error: "Não foi possível cadastrar o Usuário"}
         }
 
-        return {succes: true}
+        return {success: true}
+    }
+
+    async updateUser(id, data){
+        const dataUpdate = {}
+
+        const userValidation = await UserRepository.getUserId(id);
+        if(!userValidation){
+            return {status: 404, error: "Usuário não encontrado"}
+        }
+
+        if (data.name && data.name != userValidation.name){
+            dataUpdate.name = data.name;
+        }
+
+        if (data.email && data.email != userValidation.email){
+            if(!this.#emailValidate(data.email)){
+                return {status: 422, error: "Email inválido"}
+            }
+            else{
+                const emailVerify = await UserRepository.getUserEmail(data.email);
+                if(emailVerify){
+                    return {status: 409, error: "Email já Cadastrado"}
+                }
+                else{
+                    dataUpdate.email = data.email;
+                }
+            }
+        }
+
+        if(data.password){
+            const validPassword = await bcrypt.compare(data.password, userValidation.password)
+            if(validPassword){
+                return {status: 500, error: "A senha não pode ser igual a anterior"}
+            }
+            else{
+                dataUpdate.password = await bcrypt.hash(data.password, 10);
+            }
+        }
+
+        if(Object.keys(dataUpdate).length > 0){
+            const result = await UserRepository.updateUser(id, dataUpdate);
+
+            if(!result.success){
+                return {status: 500, error: "Não foi possível cadastrar o Usuário"}
+            }
+            return {success: true}
+        }
+        return {status: 500, error: "Dados iguais! Sem necessidade de alteração"}
+    }
+
+    async updateUserStatus(id){
+        const dataUpdate = {}
+        const user = await UserRepository.getUserId(id);
+        if(!user){
+            return {status: 404, error: "Usuário não encontrado"}
+        }
+
+        if(user.status == true){
+            dataUpdate.status = false;
+        }
+        else{
+            dataUpdate.status = true;
+        }
+
+        const result = await UserRepository.updateUser(id, dataUpdate)
+
+        if(!result.success){
+            return {status: 500, error: "Não foi possível cadastrar o Usuário"}
+        }
+        return {success: true}
     }
 
     #emailValidate(email) {
